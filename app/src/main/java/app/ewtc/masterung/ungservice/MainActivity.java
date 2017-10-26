5e6d7a2f5661e5c86c8e27b4e7384cf9753e9ee2
MasterUNG <phrombutr@gmail.com>
2017-10-27 01:37:37 +0700

    Initial commit

---
+++ app/src/main/java/app/ewtc/masterung/ungservice/MainActivity.java
@@ -0,0 +1,21 @@
+package app.ewtc.masterung.ungservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.ewtc.masterung.ungservice.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+        }
+
+    }
+}

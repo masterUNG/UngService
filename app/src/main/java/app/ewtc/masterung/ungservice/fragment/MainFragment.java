848dae1bf4d75f346f76f650ddbb96459cf52389
MasterUNG <phrombutr@gmail.com>
2017-11-01 03:26:25 +0700

    Authen email ok

--- app/src/main/java/app/ewtc/masterung/ungservice/fragment/MainFragment.java
+++ app/src/main/java/app/ewtc/masterung/ungservice/fragment/MainFragment.java
@@ -1,11 +1,20 @@
 package app.ewtc.masterung.ungservice.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import app.ewtc.masterung.ungservice.R;
 
@@ -14,6 +23,63 @@ import app.ewtc.masterung.ungservice.R;
  */
 
 public class MainFragment extends Fragment{
+
+    private String userString, passwordString;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Login Controller
+        loginController();
+
+    }
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                if (!userString.isEmpty() && !passwordString.isEmpty()) {
+//                    No Space
+                    checkUserAnPass();
+                } else {
+//                    Have Space
+                    Toast.makeText(getActivity(), "Have Space", Toast.LENGTH_SHORT).show();
+                }
+
+            }   // onClick
+        });
+    }
+
+    private void checkUserAnPass() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(userString, passwordString)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+//                        Login Success
+                        Toast.makeText(getActivity(), "Welcome User", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+//                Login False
+                Toast.makeText(getActivity(), "Login False", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -22,4 +88,4 @@ public class MainFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
-}
+}   // Main Class
